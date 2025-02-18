35450cba520d749958a5cec7b23fbc65f478be9e
Sunday Solomon <nomolos2019@gmail.com>
2025-02-18 02:48:14 +0100

    Feat: Added page responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { FaFacebook, FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import React from "react";
